859d1bb95d9e081e52950131f73d171ed0a4ae66
schneiderautomatyka <schneiderautomatyka@gmail.com>
2023-03-19 18:51:19 +0100

    Adding uis

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
 import sys
 from PyQt6.QtWidgets import QApplication, QMainWindow
 from PyQt6.QtGui import QIcon
-
+from PyQt6.uic.load_ui import loadUi
+import pyqtgraph as pg
 
 
 class MainWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         
-        # Set options
-        self.setGeometry(100, 100, 1000, 600)
-        self.setWindowTitle("Physicochemical Calculator")
-        self.setWindowIcon(QIcon("icons/chemistry_1.png"))
+        loadUi("UI/ui_main.ui", self)
         self.show()
         
         
