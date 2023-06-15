81be4b704595d316810b59a902ae044f6f31f08c
schneiderautomatyka <schneiderautomatyka@gmail.com>
2023-06-15 10:43:46 +0200

    Translation files

---
+++ UI/de.ts
@@ -0,0 +1,289 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE" sourcelanguage="en_US">
+<context>
+    <name>Calculator</name>
+    <message>
+        <location filename="ui_main.ui" line="14"/>
+        <source>Physicochemical Calculator</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_main.ui" line="44"/>
+        <source>Data</source>
+        <translation>Daten</translation>
+    </message>
+    <message>
+        <location filename="ui_main.ui" line="52"/>
+        <source>CMC</source>
+        <translation>CMC</translation>
+    </message>
+    <message>
+        <location filename="ui_main.ui" line="73"/>
+        <source>Languages</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_main.ui" line="81"/>
+        <source>Help</source>
+        <translation>Hilfe</translation>
+    </message>
+    <message>
+        <location filename="ui_main.ui" line="87"/>
+        <source>Report</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_main.ui" line="93"/>
+        <source>Settings</source>
+        <translation>Einstellungen</translation>
+    </message>
+    <message>
+        <location filename="ui_main.ui" line="110"/>
+        <source>English</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="ui_main.ui" line="119"/>
+        <source>Deutsch</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="ui_main.ui" line="128"/>
+        <source>Polski</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_main.ui" line="133"/>
+        <source>Welcome</source>
+        <translation>Wilkommen</translation>
+    </message>
+    <message>
+        <location filename="ui_main.ui" line="138"/>
+        <source>Generate ...</source>
+        <translation type="unfinished">Generieren...</translation>
+    </message>
+    <message>
+        <location filename="ui_main.ui" line="143"/>
+        <source>Graph Customization...</source>
+        <translation>Graph-Anpassung...</translation>
+    </message>
+    <message>
+        <location filename="ui_main.ui" line="148"/>
+        <source>Graph Options...</source>
+        <translation>Graph-Optionen</translation>
+    </message>
+</context>
+<context>
+    <name>Form</name>
+    <message>
+        <location filename="ui_settings_graph_options.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_settings_graph_options.ui" line="21"/>
+        <source>Emission Spectrum</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_settings_graph_options.ui" line="26"/>
+        <source>CMC</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_settings_graph_options.ui" line="51"/>
+        <source>Labels</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_settings_graph_options.ui" line="81"/>
+        <source>Left</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_settings_graph_options.ui" line="115"/>
+        <source>Bottom</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_settings_graph_options.ui" line="149"/>
+        <location filename="ui_settings_graph_options.ui" line="245"/>
+        <source>Font Size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_settings_graph_options.ui" line="187"/>
+        <location filename="ui_settings_graph_options.ui" line="211"/>
+        <source>Title</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_settings_graph_options.ui" line="283"/>
+        <source>Legend</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_settings_graph_options.ui" line="307"/>
+        <source>Legend On/Off</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_settings_graph_options.ui" line="341"/>
+        <source>Legend Text Size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_settings_graph_options.ui" line="379"/>
+        <source>Font Color</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_settings_graph_options.ui" line="425"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_settings_graph_options.ui" line="432"/>
+        <source>Apply</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SettingsDataGraph</name>
+    <message>
+        <location filename="ui_settings_graph_data.ui" line="32"/>
+        <source>Graph Customization</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_settings_graph_data.ui" line="56"/>
+        <source>Style sheet</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_settings_graph_data.ui" line="64"/>
+        <source>Random</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_settings_graph_data.ui" line="69"/>
+        <source>Grayscale</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_settings_graph_data.ui" line="77"/>
+        <source>Fill symbol</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_settings_graph_data.ui" line="104"/>
+        <source>Restore default</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_settings_graph_data.ui" line="165"/>
+        <source>Apply to column</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_settings_graph_data.ui" line="185"/>
+        <source>Reset</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_settings_graph_data.ui" line="192"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_settings_graph_data.ui" line="199"/>
+        <source>Apply</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>WidgetCAC</name>
+    <message>
+        <location filename="ui_cac.ui" line="26"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_cac.ui" line="66"/>
+        <source>Method</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_cac.ui" line="86"/>
+        <source>*</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_cac.ui" line="129"/>
+        <source>CMC mg/ml</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_cac.ui" line="192"/>
+        <source>Export</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_cac.ui" line="238"/>
+        <source>Log Scale</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>WidgetData</name>
+    <message>
+        <location filename="ui_data.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_data.ui" line="54"/>
+        <source>Concentration</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_data.ui" line="74"/>
+        <source>*</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_data.ui" line="116"/>
+        <source>I1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_data.ui" line="149"/>
+        <source>I3</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>WidgetNavigation</name>
+    <message>
+        <location filename="ui_navigation.ui" line="26"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_navigation.ui" line="60"/>
+        <source>Load Data</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_navigation.ui" line="79"/>
+        <source>Clear Data</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_navigation.ui" line="90"/>
+        <source>Path</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
